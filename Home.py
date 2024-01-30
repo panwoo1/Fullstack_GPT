5afe481fb5f8adc3dc66483a50ca9d5ebc70fe97
chanwooLim <hvcloc@gmail.com>
2024-01-30 20:50:38 +0900

    # DocumentGPT

--- Home.py
+++ Home.py
@@ -1,11 +1,20 @@
 import streamlit as st
 
-st.title("Hello world!")
-
-st.subheader("Welcome to Streamlit")
+st.set_page_config(page_title="FullstackGPT Home", page_icon="🙂")
 
 st.markdown(
     """
-    ##### I love
+    # Hello!
+    
+    Welcone to my FullstackGPT Portfolio!
+    
+    Here are the apps I mad:
+    
+    - [] [DocumentGPT](/DocumentGPT)
+    - [] [PrivateGPT](/PrivateGPT)
+    - [] [QuizGPT](/QuizGPT)
+    - [] [SiteGPT](/SiteGPT)
+    - [] [MeetingGPT](/MeetingGPT)
+    - [] [InvestorGPT](/InvestorGPT)
     """
 )
