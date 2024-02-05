732c88525d64b093bcbea1d2b06a52a4952550b8
chanwooLim <hvcloc@gmail.com>
2024-02-06 01:13:08 +0900

    # CHEFGPT - OAuth

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from typing import Any, Dict
+
+from fastapi import Body, FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, Field
 
 app = FastAPI(
@@ -6,8 +9,8 @@ app = FastAPI(
     description="Get a real quote said by Nicolacus Maximus himself.",
     servers=[
         {
-            "url": "https://introduced-charleston-widespread-literally.trycloudflare.com",
-        }
+            "url": "https://rage-adapter-gtk-wooden.trycloudflare.com",
+        },
     ],
 )
 
@@ -28,11 +31,40 @@ class Quote(BaseModel):
     response_description="A Quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said.",
     response_model=Quote,
     openapi_extra={
-        "x-openai-isConsequential": True,
+        "x-openai-isConsequential": False,
     },
 )
-def get_quote():
+def get_quote(request: Request):
+    print(request.headers)
     return {
         "quote": "Life is short so eat it all.",
         "year": 1950,
+    }
+
+
+user_token_db = {"ABCDEF": "nico"}
+
+
+@app.get(
+    "/authorize",
+    response_class=HTMLResponse,
+)
+def handle_authorize(client_id: str, redirect_uri: str, state: str):
+    return f"""
+    <html>
+        <head>
+            <title>Nicolacus Maximus Log In</title>
+        </head>
+        <body>
+            <h1>Log Into Nicolacus Maximus</h1>
+            <a href="{redirect_uri}?code=ABCDEF&state={state}">Authorize Nicolacus Maximus GPT</a>
+        </body>
+    </html>
+    """
+
+
+@app.post("/token")
+def handle_token(code=Form(...)):
+    return {
+        "access_token": user_token_db[code],
     }
